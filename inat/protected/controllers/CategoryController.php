71c3eb6182f722e24b57c3e23e29c868891e5764
高宇 <gaoyu@maitall.com>
2014-11-10 15:40:50 +0800

    修改项目category分支

--- inat/protected/controllers/CategoryController.php
+++ inat/protected/controllers/CategoryController.php
@@ -7,7 +7,7 @@ class CategoryController extends Controller
 	 * using two-column layout. See 'protected/views/layouts/column2.php'.
 	 */
 	public $layout='//layouts/column2';
-        //修改分支
+        //修改分支111
 	/**
 	 * @return array action filters
 	 */
