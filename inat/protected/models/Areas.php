10fbef31cce56e857aca452e0293f93e60ed4456
高宇 <gaoyu@maitall.com>
2014-11-12 11:01:13 +0800

    areas model 类方法明细和参数(地区属性)

--- inat/protected/models/Areas.php
+++ inat/protected/models/Areas.php
@@ -9,94 +9,124 @@
  * @property string $area_name
  * @property integer $sort
  */
-class Areas extends CActiveRecord
-{
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'areas';
-	}
+class Areas extends CActiveRecord {
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('parent_id, area_name', 'required'),
-			array('sort', 'numerical', 'integerOnly'=>true),
-			array('parent_id', 'length', 'max'=>10),
-			array('area_name', 'length', 'max'=>50),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('area_id, parent_id, area_name, sort', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+        return 'areas';
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('parent_id, area_name', 'required'),
+            array('sort', 'numerical', 'integerOnly' => true),
+            array('parent_id', 'length', 'max' => 10),
+            array('area_name', 'length', 'max' => 50),
+            // The following rule is used by search().
+            // @todo Please remove those attributes that should not be searched.
+            array('area_id, parent_id, area_name, sort', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'area_id' => 'Area',
-			'parent_id' => '上一级的id值',
-			'area_name' => '地区名称',
-			'sort' => '排序',
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * Typical usecase:
-	 * - Initialize the model fields with values from filter form.
-	 * - Execute this method to get CActiveDataProvider instance which will filter
-	 * models according to data in model fields.
-	 * - Pass data provider to CGridView, CListView or any similar widget.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// @todo Please modify the following code to remove attributes that should not be searched.
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+        return array(
+            'area_id' => 'Area',
+            'parent_id' => '上一级的id值',
+            'area_name' => '地区名称',
+            'sort' => '排序',
+        );
+    }
 
-		$criteria=new CDbCriteria;
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     * based on the search/filter conditions.
+     */
+    public function search() {
+        // @todo Please modify the following code to remove attributes that should not be searched.
 
-		$criteria->compare('area_id',$this->area_id,true);
-		$criteria->compare('parent_id',$this->parent_id,true);
-		$criteria->compare('area_name',$this->area_name,true);
-		$criteria->compare('sort',$this->sort);
+        $criteria = new CDbCriteria;
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        $criteria->compare('area_id', $this->area_id, true);
+        $criteria->compare('parent_id', $this->parent_id, true);
+        $criteria->compare('area_name', $this->area_name, true);
+        $criteria->compare('sort', $this->sort);
 
-	/**
-	 * Returns the static model of the specified AR class.
-	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
-	 * @return Areas the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
+
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return Areas the static model class
+     */
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
+
+    /**
+     * 取得子地区列表
+     *
+     * @param int $parent_id 大于等于0表示取某个地区的下级地区，小于0表示取所有地区
+     * @return array
+     */
+    function getChild($parent_id = -1) {
+        
+    }
+
+    /*
+     * 判断名称是否唯一
+     */
+
+    function unique($region_name, $parent_id, $id = 0) {
+        
+    }
+
+    /**
+     * 取得某地区的所有子孙地区id
+     */
+    function getDescendant($id) {
+        
+    }
+
+    /**
+     * 取得某地区的所有父级地区
+     *
+     * @param  int $id
+     * @return array
+     * */
+    function getParents($id) {
+        
+    }
+
 }
