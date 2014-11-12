bf8ffb3e67e0f34887b08f956ed46029840f6fe9
高宇 <gaoyu@maitall.com>
2014-11-12 15:06:04 +0800

    cart 订单管理

--- inat/protected/models/Cart.php
+++ inat/protected/models/Cart.php
@@ -16,109 +16,154 @@
  * @property integer $created
  * @property integer $updated
  */
-class Cart extends CActiveRecord
-{
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'cart';
-	}
+class Cart extends CActiveRecord {
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('carrier_id, delivery_option, uid, created, updated', 'required'),
-			array('recyclable, gift, allow_seperated_package, created, updated', 'numerical', 'integerOnly'=>true),
-			array('carrier_id, uid', 'length', 'max'=>10),
-			array('secure_key', 'length', 'max'=>32),
-			array('gift_message', 'safe'),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('id, carrier_id, delivery_option, uid, secure_key, recyclable, gift, gift_message, allow_seperated_package, created, updated', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+        return 'cart';
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('carrier_id, delivery_option, uid, created, updated', 'required'),
+            array('recyclable, gift, allow_seperated_package, created, updated', 'numerical', 'integerOnly' => true),
+            array('carrier_id, uid', 'length', 'max' => 10),
+            array('secure_key', 'length', 'max' => 32),
+            array('gift_message', 'safe'),
+            // The following rule is used by search().
+            // @todo Please remove those attributes that should not be searched.
+            array('id, carrier_id, delivery_option, uid, secure_key, recyclable, gift, gift_message, allow_seperated_package, created, updated', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => '编号',
-			'carrier_id' => '购物车编号',
-			'delivery_option' => '物流方式',
-			'uid' => '用户编号',
-			'secure_key' => '密钥',
-			'recyclable' => '回收状态',
-			'gift' => '礼品编号',
-			'gift_message' => '礼品消息',
-			'allow_seperated_package' => '是否允许分包',
-			'created' => '创建时间',
-			'updated' => '修改时间',
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * Typical usecase:
-	 * - Initialize the model fields with values from filter form.
-	 * - Execute this method to get CActiveDataProvider instance which will filter
-	 * models according to data in model fields.
-	 * - Pass data provider to CGridView, CListView or any similar widget.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// @todo Please modify the following code to remove attributes that should not be searched.
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+        return array(
+            'id' => '编号',
+            'carrier_id' => '购物车编号',
+            'delivery_option' => '物流方式',
+            'uid' => '用户编号',
+            'secure_key' => '密钥',
+            'recyclable' => '回收状态',
+            'gift' => '礼品编号',
+            'gift_message' => '礼品消息',
+            'allow_seperated_package' => '是否允许分包',
+            'created' => '创建时间',
+            'updated' => '修改时间',
+        );
+    }
 
-		$criteria=new CDbCriteria;
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     * based on the search/filter conditions.
+     */
+    public function search() {
+        // @todo Please modify the following code to remove attributes that should not be searched.
 
-		$criteria->compare('id',$this->id,true);
-		$criteria->compare('carrier_id',$this->carrier_id,true);
-		$criteria->compare('delivery_option',$this->delivery_option,true);
-		$criteria->compare('uid',$this->uid,true);
-		$criteria->compare('secure_key',$this->secure_key,true);
-		$criteria->compare('recyclable',$this->recyclable);
-		$criteria->compare('gift',$this->gift);
-		$criteria->compare('gift_message',$this->gift_message,true);
-		$criteria->compare('allow_seperated_package',$this->allow_seperated_package);
-		$criteria->compare('created',$this->created);
-		$criteria->compare('updated',$this->updated);
+        $criteria = new CDbCriteria;
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        $criteria->compare('id', $this->id, true);
+        $criteria->compare('carrier_id', $this->carrier_id, true);
+        $criteria->compare('delivery_option', $this->delivery_option, true);
+        $criteria->compare('uid', $this->uid, true);
+        $criteria->compare('secure_key', $this->secure_key, true);
+        $criteria->compare('recyclable', $this->recyclable);
+        $criteria->compare('gift', $this->gift);
+        $criteria->compare('gift_message', $this->gift_message, true);
+        $criteria->compare('allow_seperated_package', $this->allow_seperated_package);
+        $criteria->compare('created', $this->created);
+        $criteria->compare('updated', $this->updated);
 
-	/**
-	 * Returns the static model of the specified AR class.
-	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
-	 * @return Cart the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
+
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return Cart the static model class
+     */
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
+
+    /**
+     * 删除购物车
+     *
+     * @return bool
+     */
+    public function delete($id) {
+        
+    }
+
+    /**
+     * 修改购物车
+     *
+     * @return bool
+     */
+    public function update($cart) {
+        
+    }
+
+    /**
+     * 得到购物车产品列表
+     *
+     * @return array
+     */
+    public function getProductsById($id) {
+        
+    }
+
+    /**
+     * 得到购物车产品数量
+     *
+     * @return int
+     */
+    public function getGoodCount($id) {
+        
+    }
+
+    /**
+     * 得到购物车产品数量
+     *
+     * @return int
+     */
+    public function deleteGood($id) {
+        
+    }
+
+    //清空购物车
+    public function clear() {
+        
+    }
+
 }
